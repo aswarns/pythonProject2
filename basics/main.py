e7f2809318e2886fc915cfcd9452f253b5070bf3
aswarns <a9ever@gmail.com>
2023-10-31 00:32:46 +0530

    updtes

--- basics/main.py
+++ basics/main.py
@@ -1,5 +1,26 @@
+#BMI Calculator
+height = float(input("Enter your height in m: "))
+weight =  float(input("Enter your weight in kg: "))
 
-var1 = input("Enter the name..")
-var2= len(var1)
-print("Input:", var1)
-print("Outout:", var2)
+bmi = round(weight/(height*height), 2)
+
+if  bmi < 18.5:
+    print(f"ur BMI: {bmi}, ur UNDEWEIGHT")
+elif bmi < 25:
+    print(f"ur BMI: {bmi}, ur NORMAL")
+elif bmi < 30:
+    print(f"ur BMI: {bmi}, ur OVERWEIGHT")
+else:
+    print(f"ur BMI: {bmi}, ur OBESE")
+
+'''
+
+if  bmi > 30 and bmi < 35:
+    print(f"ur BMI: {bmi}, ur OBESE")
+elif bmi > 25:
+    print(f"ur BMI: {bmi}, ur overweight")
+elif bmi >= 18.5:
+    print(f"ur BMI: {bmi}, ur NORMAL")
+else:
+    print(f"ur BMI: {bmi}, ur UNDERweight")
+'''
